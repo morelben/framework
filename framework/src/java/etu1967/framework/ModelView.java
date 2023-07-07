d648068a05b1770f3600219339837a934525150b
Jimmy2038 <119540791+Jimmy2038@users.noreply.github.com>
2023-07-07 23:55:15 +0200

    Sprint8

--- framework/src/java/etu1967/framework/ModelView.java
+++ framework/src/java/etu1967/framework/ModelView.java
@@ -5,19 +5,31 @@
  */
 package etu1967.framework;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Best
  */
 public class ModelView {
     String view;
+    HashMap<String, Object> session = new HashMap<>();
 
     public String getView() {
         return view;
     }
-
+    
     public void setView(String view) {
         this.view = view;
+    }
+    public void setSession(HashMap<String, Object> session) {
+        this.session = session;
+    }
+    public HashMap<String, Object> getSession() {
+        return session;
+    }
+    public void addSession(String key,Object object) {
+        this.session.put(key,object);
     }
     
 }
